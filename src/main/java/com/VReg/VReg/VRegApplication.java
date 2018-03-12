71c7d987d9451c6e1662c38bb5225d98ee823114
DinakarNR <naramala.dinakar@appistoki.com>
2018-03-12 14:54:14 +0530

    Basic setup VReg.

---
+++ src/main/java/com/VReg/VReg/VRegApplication.java
@@ -0,0 +1,12 @@
+package com.VReg.VReg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VRegApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VRegApplication.class, args);
+	}
+}
